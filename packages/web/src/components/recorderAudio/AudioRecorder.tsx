20cc4c2e136cd66f50b96e6aaae6b8793658f7ce
fzk <458813868@qq.com>
2023-10-17 14:14:12 +0800

    refactor: home页文字错误,波形图选择

--- packages/web/src/components/recorderAudio/AudioRecorder.tsx
+++ packages/web/src/components/recorderAudio/AudioRecorder.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useRef, useEffect, useCallback } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import { Radio, Card, Divider } from "antd";
+import { Radio, Card, Divider, Switch, Space } from "antd";
 import WaveSurfer from "wavesurfer.js";
 import RecordPlugin from "./plugins/recordPlugin";
 import dayjs from "dayjs";
@@ -68,31 +68,48 @@ const AudioRecorder = (props) => {
 		record.resumeRecording();
 	}
 
+	function changeMic(checked) {
+		checked ? openRecord() : destroyRecord();
+	}
+
 	return (
 		<Card title="设置">
-			<Radio.Group buttonStyle="solid">
-				<Radio.Button value="open" onClick={openRecord}>
-					打开麦克风
-				</Radio.Button>
-				<Radio.Button value="destroy" onClick={destroyRecord}>
-					关闭麦克风
-				</Radio.Button>
-			</Radio.Group>
+			<Space>
+				麦克风
+				<Switch
+					checkedChildren="开启"
+					unCheckedChildren="关闭"
+					onChange={changeMic}
+				/>
+			</Space>
 			<Divider />
-			<Radio.Group buttonStyle="solid" disabled={isDisabled} value={value}>
-				<Radio.Button value="start" onClick={startRecord}>
-					开始
-				</Radio.Button>
-				<Radio.Button value="stop" onClick={stopRecord}>
-					保存
-				</Radio.Button>
-				<Radio.Button value="pause" onClick={pauseRecord}>
-					暂停
-				</Radio.Button>
-				<Radio.Button value="resume" onClick={resumeRecord}>
-					继续
-				</Radio.Button>
-			</Radio.Group>
+			<Space>
+				操作
+				<Radio.Group buttonStyle="solid" disabled={isDisabled} value={value}>
+					<Radio.Button value="start" onClick={startRecord}>
+						开始
+					</Radio.Button>
+					<Radio.Button value="stop" onClick={stopRecord}>
+						保存
+					</Radio.Button>
+					<Radio.Button value="pause" onClick={pauseRecord}>
+						暂停
+					</Radio.Button>
+					<Radio.Button value="resume" onClick={resumeRecord}>
+						继续
+					</Radio.Button>
+				</Radio.Group>
+				声波
+				<Radio.Group buttonStyle="solid" value={"WaveView"}>
+					<Radio.Button value="WaveView">WaveView</Radio.Button>
+					<Radio.Button value="SurferView" disabled>
+						SurferView
+					</Radio.Button>
+					<Radio.Button value="Histogram" disabled>
+						Histogram
+					</Radio.Button>
+				</Radio.Group>
+			</Space>
 			<Divider />
 			<div id="mic" ref={micRef}></div>
 		</Card>
