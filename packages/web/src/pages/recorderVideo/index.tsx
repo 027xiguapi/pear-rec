d266cd63e085c319d6bf7ec826b8fd98c85765b0
fzk <458813868@qq.com>
2024-04-13 11:14:15 +0800

    perf: 修改背景图片

--- packages/web/src/pages/recorderVideo/index.tsx
+++ packages/web/src/pages/recorderVideo/index.tsx
@@ -8,6 +8,7 @@ import { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { db, defaultUser } from '../../db';
 import initApp from '../main';
+import styles from './index.module.scss';
 
 let avCvs: AVCanvas | null = null;
 let recorder: AVRecorder | null = null;
@@ -121,109 +122,115 @@ export default function UI() {
   }
 
   return (
-    <Card>
-      <Flex gap="small" wrap="wrap">
-        添加素材：
-        <Button
-          onClick={async () => {
-            if (avCvs == null) return;
-            const mediaStream = await navigator.mediaDevices.getUserMedia({
-              video: true,
-              audio: true,
-            });
-            const vs = new VideoSprite('userMedia', mediaStream, {
-              audioCtx: avCvs.spriteManager.audioCtx,
-            });
-            await avCvs.spriteManager.addSprite(vs);
-          }}
-        >
-          摄像 & 麦克风
-        </Button>
-        <Button
-          onClick={async () => {
-            if (avCvs == null) return;
-            const mediaStream = await navigator.mediaDevices.getDisplayMedia({
-              video: true,
-              audio: true,
-            });
-            const vs = new VideoSprite('display', mediaStream, {
-              audioCtx: avCvs.spriteManager.audioCtx,
-            });
-            await avCvs.spriteManager.addSprite(vs);
-          }}
-        >
-          屏幕
-        </Button>
-        <Button
-          onClick={async () => {
-            if (avCvs == null) return;
-            const is = new ImgSprite(
-              'img',
-              await loadFile({ 'image/*': ['.png', '.gif', '.jpeg', '.jpg'] }),
-            );
-            await avCvs.spriteManager.addSprite(is);
-          }}
-        >
-          图片
-        </Button>
-        <Button
-          onClick={async () => {
-            if (avCvs == null) return;
-            const vs = new VideoSprite('video', await loadFile({ 'video/*': ['.webm', '.mp4'] }), {
-              audioCtx: avCvs.spriteManager.audioCtx,
-            });
-            await avCvs.spriteManager.addSprite(vs);
-          }}
-        >
-          视频
-        </Button>
-        <Button
-          onClick={async () => {
-            if (avCvs == null) return;
-            const as = new AudioSprite(
-              'audio',
-              await loadFile({ 'audio/*': ['.mp3', '.wav', '.ogg'] }),
-              { audioCtx: avCvs.spriteManager.audioCtx },
-            );
-            await avCvs.spriteManager.addSprite(as);
-          }}
-        >
-          音频
-        </Button>
-        <Button
-          onClick={async () => {
-            if (avCvs == null) return;
-            const fs = new TextSprite('text', '示例文字');
-            await avCvs.spriteManager.addSprite(fs);
-          }}
-        >
-          文字
-        </Button>
-      </Flex>
-      <Divider />
-      <Flex gap="small" wrap="wrap">
-        <Button
-          onClick={async () => {
-            await start();
-          }}
-        >
-          开始录制
-        </Button>
-        <Button
-          onClick={async () => {
-            await recorder?.stop();
-            setStateText('视频已保存');
-          }}
-        >
-          停止录制
-        </Button>
-        <span style={{ marginLeft: 16, color: '#666' }}>{stateText}</span>
-      </Flex>
-      <div
-        ref={initCvs}
-        style={{ width: 900, height: 500, position: 'relative', marginTop: 20 }}
-      ></div>
-    </Card>
+    <div className={styles.recorderVideo}>
+      <Card className="content">
+        <Flex gap="small" wrap="wrap" className="tool">
+          添加素材：
+          <Button
+            onClick={async () => {
+              if (avCvs == null) return;
+              const mediaStream = await navigator.mediaDevices.getUserMedia({
+                video: true,
+                audio: true,
+              });
+              const vs = new VideoSprite('userMedia', mediaStream, {
+                audioCtx: avCvs.spriteManager.audioCtx,
+              });
+              await avCvs.spriteManager.addSprite(vs);
+            }}
+          >
+            摄像 & 麦克风
+          </Button>
+          <Button
+            onClick={async () => {
+              if (avCvs == null) return;
+              const mediaStream = await navigator.mediaDevices.getDisplayMedia({
+                video: true,
+                audio: true,
+              });
+              const vs = new VideoSprite('display', mediaStream, {
+                audioCtx: avCvs.spriteManager.audioCtx,
+              });
+              await avCvs.spriteManager.addSprite(vs);
+            }}
+          >
+            屏幕
+          </Button>
+          <Button
+            onClick={async () => {
+              if (avCvs == null) return;
+              const is = new ImgSprite(
+                'img',
+                await loadFile({ 'image/*': ['.png', '.gif', '.jpeg', '.jpg'] }),
+              );
+              await avCvs.spriteManager.addSprite(is);
+            }}
+          >
+            图片
+          </Button>
+          <Button
+            onClick={async () => {
+              if (avCvs == null) return;
+              const vs = new VideoSprite(
+                'video',
+                await loadFile({ 'video/*': ['.webm', '.mp4'] }),
+                {
+                  audioCtx: avCvs.spriteManager.audioCtx,
+                },
+              );
+              await avCvs.spriteManager.addSprite(vs);
+            }}
+          >
+            视频
+          </Button>
+          <Button
+            onClick={async () => {
+              if (avCvs == null) return;
+              const as = new AudioSprite(
+                'audio',
+                await loadFile({ 'audio/*': ['.mp3', '.wav', '.ogg'] }),
+                { audioCtx: avCvs.spriteManager.audioCtx },
+              );
+              await avCvs.spriteManager.addSprite(as);
+            }}
+          >
+            音频
+          </Button>
+          <Button
+            onClick={async () => {
+              if (avCvs == null) return;
+              const fs = new TextSprite('text', '示例文字');
+              await avCvs.spriteManager.addSprite(fs);
+            }}
+          >
+            文字
+          </Button>
+        </Flex>
+        <Divider />
+        <Flex gap="small" wrap="wrap">
+          <Button
+            onClick={async () => {
+              await start();
+            }}
+          >
+            开始录制
+          </Button>
+          <Button
+            onClick={async () => {
+              await recorder?.stop();
+              setStateText('视频已保存');
+            }}
+          >
+            停止录制
+          </Button>
+          <span style={{ marginLeft: 16, color: '#666' }}>{stateText}</span>
+        </Flex>
+        <div
+          ref={initCvs}
+          style={{ width: 900, height: 500, position: 'relative', marginTop: 20 }}
+        ></div>
+      </Card>
+    </div>
   );
 }
 
